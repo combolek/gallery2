2fe989b770a24c73a848ca6d7ad430899b1ca5a0
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-01-07 21:19:07 +0000

    User management pages for the admin.

---
+++ gallery/create_user.php
@@ -0,0 +1,85 @@
+<?
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000 Bharat Mediratta
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+?>
+<?
+if (!$user->isAdmin()) {
+	exit;	
+}
+
+if ($submit) {
+	if (!strcmp($submit, "Create")) {
+		$gErrors["uname"] = $userDB->validNewUserName($uname);
+		if ($gErrors["uname"]) {
+			$errorCount++;
+		}
+
+		$gErrors["password"] = $userDB->validPassword($password);
+		if ($gErrors["password"]) {
+			$errorCount++;
+		}
+
+		if (!$errorCount) {
+			$tmpUser = new User();
+			$tmpUser->setUsername($uname);
+			$tmpUser->setPassword($password);
+			$tmpUser->setFullname($fullname);
+			$tmpUser->setEmail($email);
+			$tmpUser->save();
+			header("Location: manage_users.php");
+		}
+	} else if (!strcmp($submit, "Cancel")) {
+		header("Location: manage_users.php");
+	}
+}
+
+?>
+<html>
+<head>
+  <title>Create User</title>
+  <link rel="stylesheet" type="text/css" href="<?= getGalleryStyleSheetName() ?>">
+</head>
+<body>
+
+<center>
+<span class="popuphead">Create User</span>
+<br>
+<br>
+Create a new user here.
+<p>
+
+<form name=usercreate_form method=GET>
+<p>
+
+<? include("html/userData.inc"); ?>
+<p>
+
+<input type=submit name="submit" value="Create">
+<input type=submit name="submit" value="Cancel">
+</form>
+
+<script language="javascript1.2">
+<!--
+// position cursor in top form field
+document.usercreate_form.uname.focus();
+//--> 
+</script>
+
+</body>
+</html>
---
+++ gallery/modify_user.php
@@ -0,0 +1,104 @@
+<?
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000 Bharat Mediratta
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+?>
+<?
+if (!$user->isAdmin()) {
+	exit;	
+}
+
+if ($submit) {
+	if (!strcmp($submit, "Save")) {
+		if (strcmp($old_uname, $uname)) {
+			$gErrors["uname"] = $userDB->validNewUserName($uname);
+			if ($gErrors["uname"]) {
+				$errorCount++;
+			}
+		}
+
+		if ($password) {
+			$gErrors["password"] = $userDB->validPassword($password);
+			if ($gErrors["password"]) {
+				$errorCount++;
+			}
+		}
+
+		if (!$errorCount) {
+			$tmpUser = $userDB->getUser($old_uname);
+			$tmpUser->setUsername($uname);
+			$tmpUser->setFullname($fullname);
+			$tmpUser->setEmail($email);
+
+			// If a new password was entered, use it.  Otherwise leave
+			// it the same.
+			if ($password) {
+				$tmpUser->setPassword($password);
+			}
+			$tmpUser->save();
+			header("Location: manage_users.php");
+		}
+	} else if (!strcmp($submit, "Cancel")) {
+		header("Location: manage_users.php");
+	}
+}
+
+$tmpUser = $userDB->getUser($uname);
+if (!$tmpUser) {
+	error("Invalid user <i>$uname</i>");
+	exit;
+}
+
+$fullname = $tmpUser->getFullname();
+$email = $tmpUser->getEmail();
+
+?>
+<html>
+<head>
+  <title>Modify User</title>
+  <link rel="stylesheet" type="text/css" href="<?= getGalleryStyleSheetName() ?>">
+</head>
+<body>
+
+<center>
+<span class="popuphead">Modify User</span>
+<br>
+<br>
+You can change any information about the user using this form.
+<p>
+
+<form name=usermodify_form method=GET>
+<input type=hidden name=old_uname value=<?=$uname?>>
+<p>
+
+<? include("html/userData.inc"); ?>
+<p>
+
+<input type=submit name="submit" value="Save">
+<input type=submit name="submit" value="Cancel">
+</form>
+
+<script language="javascript1.2">
+<!--
+// position cursor in top form field
+document.usermodify_form.uname.focus();
+//--> 
+</script>
+
+</body>
+</html>
