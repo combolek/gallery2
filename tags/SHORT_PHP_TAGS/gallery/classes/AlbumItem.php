1fdf94f82a0147580f44c8b76cea080133f95d9f
nobody <nobody@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-03-09 08:18:07 +0000

    This commit was manufactured by cvs2svn to create tag 'SHORT_PHP_TAGS'.

---
+++ tags/SHORT_PHP_TAGS/gallery/classes/AlbumItem.php
@@ -0,0 +1,254 @@
+<?
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000 Bharat Mediratta
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+?>
+<?
+class AlbumItem {
+	var $image;
+	var $thumbnail;
+	var $caption;
+	var $hidden;
+	var $highlight;
+	var $highlightImage;
+
+	function hide() {
+		$this->hidden = 1;
+	}
+
+	function unhide() {
+		$this->hidden = 0;
+	}
+
+	function isHidden() {
+		return $this->hidden;
+	}
+
+	function setHighlight($dir, $bool) {
+		global $app;
+		
+		$this->highlight = $bool;
+		
+		/*
+		 * if it is now the highlight make sure it has a highlight
+                 * thumb otherwise get rid of it's thumb (ouch!).
+		 */
+		$name = $this->image->name;
+		$tag = $this->image->type;
+		if ($this->highlight) {
+			$ret = resize_image("$dir/$name.$tag",
+					     "$dir/$name.highlight.$tag",
+					     $app->highlight_size);
+
+			if ($ret) {
+				list($w, $h) = getDimensions("$dir/$name.highlight.$tag");
+
+				$high = new Image;
+				$high->setFile($dir, "$name.highlight", "$tag");
+				$high->setDimensions($w, $h);
+				$this->highlightImage = $high;
+			}
+		}
+		else {
+			if (file_exists("$dir/$name.highlight.$tag")) {
+				unlink("$dir/$name.highlight.$tag");
+			}
+		}	
+	}
+
+	function isHighlight() {
+		return $this->highlight;
+	}
+
+	function getThumbDimensions() {
+		if ($this->thumbnail) {
+			return $this->thumbnail->getDimensions();
+		} else {
+			return array(0, 0);
+		}
+	}
+
+	function getDimensions() {
+		if ($this->image) {
+			return $this->image->getDimensions();
+		} else {
+			return array(0, 0);
+		}
+	}
+
+	function makeThumbnail($dir, $size) {
+		$name = $this->image->name;
+		$type = $this->image->type;
+
+		$this->setPhoto($dir, $name, $type, $size);
+	}
+
+	function isResized() {
+		$im = $this->image;
+		return $im->isResized();
+	}
+
+	function rotate($dir, $direction, $thumb_size) {
+		global $app;
+
+		$name = $this->image->name;
+		$type = $this->image->type;
+
+		$cmd = toPnmCmd("$dir/$name.$type") .
+			"| $app->pnmDir/pnmrotate $direction" .
+			"| " . fromPnmCmd("$dir/tmp.$type");
+		exec_wrapper($cmd);
+		
+		if (file_exists("$dir/tmp.$type") && filesize("$dir/tmp.$type") > 0) {
+			copy("$dir/tmp.$type", "$dir/$name.$type");
+			unlink("$dir/tmp.$type");
+		}
+
+		$isResized = $this->isResized();
+
+		if ($isResized) {
+			list($w, $h) = $this->image->getDimensions();			
+		}
+
+		/* And rebuild the thumbnail */
+		$this->setPhoto($dir, $name, $type, $thumb_size);
+
+		if ($isResized) {
+			$this->image->resize($dir, max($h, $w));
+		}
+	}
+
+	function setPhoto($dir, $name, $tag, $thumb_size) {
+		global $app;
+
+		/*
+	 	 * Sanity: make sure we can handle the file first.
+		 */
+		if (!strcmp($tag, "avi") &&
+		    !strcmp($tag, "mpg") && 
+		    !valid_image("$dir/$name.$tag")) {
+			return "Invalid image: $name.$tag";
+		}
+
+		/* Set our image. */
+		$this->image = new Image;
+		$this->image->setFile($dir, $name, $tag);
+
+		if (!strcmp($tag, "avi") || !strcmp($tag, "mpg")) {
+			/* Use a preset thumbnail */
+			copy($app->movieThumbnail, "$dir/$name.thumb.jpg");
+			$this->thumbnail = new Image;
+			$this->thumbnail->setFile($dir, "$name.thumb", "jpg");
+
+			list($w, $h) = getDimensions("$dir/$name.thumb.jpg");
+			$this->thumbnail->setDimensions($w, $h);
+		} else {
+			list($w, $h) = getDimensions("$dir/$name.$tag");
+			if ($w != 0 && $h != 0) {
+				$this->image->setDimensions($w, $h);
+			}
+
+			/* Make thumbnail */
+			$ret = resize_image("$dir/$name.$tag",
+					     "$dir/$name.thumb.$tag",
+					     $thumb_size);
+
+			if ($ret) { 
+				$this->thumbnail = new Image;
+				$this->thumbnail->setFile($dir, "$name.thumb", $tag);
+	
+				list($w, $h) = getDimensions("$dir/$name.thumb.$tag");
+				$this->thumbnail->setDimensions($w, $h);
+			} else {
+				return "Unable to make thumbnail ($ret)";
+			}
+		}
+
+		return 0;
+	}
+
+	function getThumbnailTag($dir, $attrs="") {
+		if ($this->thumbnail) {
+			return $this->thumbnail->getTag($dir, 0, $attrs);
+		} else {
+			return "<i>No thumbnail</i>";
+		}
+	}
+
+	function getHighlightTag($dir, $attrs) {
+		if (is_object($this->highlightImage)) {
+			return $this->highlightImage->getTag($dir, 0, $attrs);
+		} else {
+			return "<i>No highlight</i>";
+		}
+	}
+
+	function getPhotoTag($dir, $full=0) {
+		if ($this->image) {
+			return $this->image->getTag($dir, $full);
+		} else {
+			return "about:blank";
+		}
+	}
+
+	function getPhotoPath($dir) {
+		if ($this->image) {
+			return $this->image->getPath($dir);
+		} else {
+			return "about:blank";
+		}
+	}
+
+	function getPhotoId($dir) {
+		if ($this->image) {
+			return $this->image->getId($dir);
+		} else {
+			return "unknown";
+		}
+	}
+
+	function delete($dir) {
+		if ($this->image) {
+			$this->image->delete($dir);
+		}
+
+		if ($this->thumbnail) {
+			$this->thumbnail->delete($dir);
+		}
+	}
+
+	function setCaption($cap) {
+		$this->caption = $cap;
+	}
+
+	function getCaption() {
+		return $this->caption;
+	}
+
+	function isMovie() {
+		return isMovie($this->image->type);
+	}
+
+	function resize($dir, $target) {
+		if ($this->image) {
+			$this->image->resize($dir, $target);
+		}
+	}
+}
+
+?>
---
+++ tags/SHORT_PHP_TAGS/gallery/view_photo.php
@@ -0,0 +1,390 @@
+<?
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000 Bharat Mediratta
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+?>
+<? require_once('init.php'); ?>
+<?
+// Hack check
+if (!$user->canReadAlbum($album)) {
+	header("Location: albums.php");
+	return;
+}
+
+if ($id) {
+	$index = $album->getPhotoIndex($id);
+	if ($index == -1) {
+		// That photo no longer exists.
+		header("Location: $app->photoAlbumURL/$albumName");
+		return;
+	}
+} else {
+	$id = $album->getPhotoId($index);
+}
+$photo = $album->getPhoto($index);
+$photoURL = $album->getAlbumDirURL() . "/" . $photo->image->name . "." . $photo->image->type;
+$fitToWindow = !strcmp($album->fields["fit_to_window"], "yes") && !$album->isResized($index);
+list($imageWidth, $imageHeight) = $photo->image->getDimensions();
+
+$do_fullOnly = !strcmp($fullOnly,"on") &&
+               !strcmp($album->fields["use_fullOnly"],"yes");
+if ($do_fullOnly) {
+	$full = 1;
+}
+
+if ($full) {
+	$fullTag = "?full=1";
+}
+
+$numPhotos = $album->numPhotos($user->canWriteToAlbum($album));
+$next = $index+1;
+if ($next > $numPhotos) {
+	//$next = 1;
+        $last = 1;
+}
+$prev = $index-1;
+if ($prev <= 0) {
+	//$prev = $numPhotos;
+        $first = 1;
+}
+
+if ($index > $numPhotos) {
+	$index = $numPhotos;
+}
+
+/*
+ * We might be prev/next navigating using this page
+ *  so recalculate the 'page' variable
+ */
+$rows = $album->fields["rows"];
+$cols = $album->fields["cols"];
+$perPage = $rows * $cols;
+$page = ceil($index / ($rows * $cols));
+
+/*
+ * Relative URLs are tricky if we don't know if we're rewriting
+ * URLs or not.  If we're rewriting, then the browser will think
+ * we're down 1 dir farther than we really are.  Use absolute 
+ * urls wherever possible.
+ */
+$top = $app->photoAlbumURL;
+
+#-- if borders are off, just make them the bgcolor ----
+if (!strcmp($album->fields["border"], "off")) {
+        $bordercolor = $album->fields["bgcolor"];
+        $borderwidth = 4;
+} else {
+        $bordercolor = $album->fields["bordercolor"];
+        $borderwidth = $album->fields["border"];
+}
+
+if (!strcmp($album->fields["resize_size"], "off")) {
+        $mainWidth = 0;
+} else {
+	$mainWidth = $album->fields["resize_size"] + ($borderwidth*2);
+
+
+}
+
+$navigator["page"] = $index;
+$navigator["maxPages"] = $numPhotos;
+$navigator["fullWidth"] = "100";
+$navigator["widthUnits"] = "%";
+$navigator["url"] = ".";
+$navigator["bordercolor"] = $bordercolor;
+
+#-- breadcrumb text ---
+if (strcmp($album->fields["returnto"], "no")) {
+	$breadtext[0] = "Gallery: <a href=$top/albums.php>".$app->galleryTitle."</a>";
+	$breadtext[1] = "Album: <a href=$top/view_album.php?page=$page>".$album->fields["title"]."</a>";
+} else {
+	$breadtext[0] = "Album: <a href=$top/view_album.php?page=$page>".$album->fields["title"]."</a>";
+}
+?>
+
+<head>
+  <title><?= $app->galleryTitle ?> :: <?= $album->fields["title"] ?> :: <?= $index ?></title>
+  <link rel="stylesheet" type="text/css" href="<?= getGalleryStyleSheetName() ?>">
+  <style type="text/css">
+<?
+// the link colors have to be done here to override the style sheet
+if ($album->fields["linkcolor"]) {
+?>      
+    A:link, A:visited, A:active
+      { color: <?= $album->fields[linkcolor] ?>; }
+    A:hover
+      { color: #ff6600; }
+<? 
+}       
+if ($album->fields["bgcolor"]) {
+        echo "BODY { background-color:".$album->fields[bgcolor]."; }";
+}       
+if ($album->fields["background"]) {
+        echo "BODY { background-image:url(".$album->fields[background]."); } ";
+} 
+if ($album->fields["textcolor"]) {
+        echo "BODY, TD {color:".$album->fields[textcolor]."; }";
+	echo ".head {color:".$album->fields[textcolor]."; }";
+	echo ".headbox {background-color:".$album->fields[bgcolor]."; }";
+}       
+?> 
+  </style> 
+  <script language="javascript1.2">
+  // <!--
+
+<?
+if ($fitToWindow) { 
+?>
+
+  function fitToWindow() {
+	var changed = 0;
+	var heightMargin = 30;
+	var widthMargin = 30;
+	var imageHeight = <?=$imageHeight?>;
+	var imageWidth = <?=$imageWidth?>;
+	var aspect = imageHeight / imageWidth;
+
+	// Get the window dimensions height.  IE and Nav use different techniques.
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	if (windowWidth == undefined) {
+		windowWidth = document.body.clientWidth;
+		windowHeight = document.body.clientHeight;
+	}
+
+	// Leave a gutter around the edges
+	windowWidth = windowWidth - widthMargin;
+	windowHeight = windowHeight - heightMargin;
+
+	if (imageWidth > windowWidth) {
+		imageWidth = windowWidth;
+		imageHeight = aspect * imageWidth;
+		changed = 1;
+	} else if (imageHeight > windowHeight) {
+		imageHeight = windowHeight;
+		imageWidth = imageHeight / aspect;
+		changed = 1;
+	}
+
+	if (changed) {
+		if (document.all) {
+			// We're in IE where we can just resize the image.
+			var img = document.images.photo;
+			img.height = imageHeight;
+			img.width = imageWidth;
+		} else {
+			// In Netscape we've got to rewrite the DIV container.
+			container = document.layers["zoom"];
+			container.document.write('<img src=\"<?=$photoURL?>\" ' +
+						'width=' + imageWidth + 
+						' height=' + imageHeight + '>');
+			container.document.close();
+		}
+	}
+  }
+
+<? 
+} // if ($fitToWindow)
+?>
+
+  // -->
+  </script>
+</head>
+
+<? if ($fitToWindow && !$full) { ?>
+<body onLoad='fitToWindow()' onResize='fitToWindow()'>
+<? } else { ?>
+<body>
+<? } ?>
+
+<?
+includeHtmlWrap("photo.header");
+?>
+
+<!-- Top Nav Bar -->
+<table border=0 width=<?=$mainWidth?> cellpadding=0 cellspacing=0>
+
+<tr>
+<td>
+<?
+
+if (!$album->isMovie($index)) {
+	if ($user->canWriteToAlbum($album)) {
+		$adminCommands .= '<a href="#" onClick="'.popup("$top/resize_photo.php?index=$index").'">[resize photo]</a>';
+	}
+
+	if ($user->canDeleteFromAlbum($album)) {
+		$adminCommands .= '<a href="#" onClick="'.popup("$top/delete_photo.php?index=$index").'">[delete photo]</a>';
+	}
+
+	if (!strcmp($album->fields["use_fullOnly"], "yes")) {
+		$link = "$top/do_command.php?set_fullOnly=" .
+		        (strcmp($fullOnly,"on") ? "on" : "off") .
+		        "&return=" . urlencode($REQUEST_URI);
+		$adminCommands .= " View Images: [ ";
+		if (strcmp($fullOnly,"on"))
+		{
+			$adminCommands .= "normal | <a href=\"$link\">full</a> ]";
+		} else {
+			$adminCommands .= "<a href=\"$link\">normal</a> | full ]";
+		}
+	}
+
+	$adminbox["text"] = "&nbsp;";
+	if ($adminCommands) {
+		$adminCommands = "<span class=\"admin\">$adminCommands</span>";
+		$adminbox["commands"] = $adminCommands;
+	}
+
+	$adminbox["bordercolor"] = $bordercolor;
+	$adminbox["top"] = true;
+	include ("layout/adminbox.inc");
+}
+
+$breadcrumb["text"] = $breadtext;
+$breadcrumb["bordercolor"] = $bordercolor;
+$breadcrumb["top"] = true;
+
+include("layout/breadcrumb.inc");
+?>
+</td>
+</tr>
+<tr>
+<td>
+<?
+
+include("layout/navphoto.inc");
+?>
+<br>
+</td>
+</tr>
+
+
+</table>
+<table border=0 width=<?=$mainWidth?> cellpadding=0 cellspacing=0>
+<tr><td colspan=3>
+<?
+includeHtmlWrap("inline_photo.header");
+?>
+</td></tr>
+</table>
+
+<!-- image -->
+
+
+<?
+if ($fitToWindow) {
+	echo "<div align=left id=\"zoom\" " .
+		"style=\"height:$imageHeight; width:$imageWidth; top:0; left:0; position: relative\">";
+}
+echo("<table width=1% border=0 cellspacing=0 cellpadding=0>");
+echo("<tr bgcolor=$bordercolor>");
+echo("<td height=$borderwidth width=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("<td height=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("<td height=$borderwidth width=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("</tr>");
+echo("<tr>");
+echo("<td bgcolor=$bordercolor width=$borderwidth>");
+for ($k=0; $k<$borderwidth; $k++) {
+	echo("<img src=$top/images/pixel_trans.gif>");
+}
+echo("</td>");
+echo("<td>");
+echo "<center>";
+
+$photoTag = $album->getPhotoTag($index, $full);
+
+if (!$album->isMovie($index)) {
+	if ($fitToWindow) {
+		$photoTag = "<img name=photo src=$photoURL border=0 width=$imageWidth height=$imageHeight>";
+	} else if ($album->isResized($index) && !$do_fullOnly) { 
+		if ($full) { 
+			echo "<a href=" . makeGalleryUrl($albumName, $id) . ">";
+	 	} else {
+			echo "<a href=" . makeGalleryUrl($albumName, $id, "full=1") . ">";
+		}
+		$openAnchor = 1;
+	}
+} else {
+	echo "<a href=" . $album->getPhotoPath($index) . " target=other>";
+	$openAnchor = 1;
+}
+
+echo $photoTag;
+
+if ($openAnchor) {
+	echo "</a>";
+ 	$openAnchor = 0;
+}
+
+echo("</td>");
+echo("<td bgcolor=$bordercolor width=$borderwidth>");
+for ($k=0; $k<$borderwidth; $k++) {
+	echo("<img src=$top/images/pixel_trans.gif>");
+}
+echo("</td>");
+echo("</tr>");
+echo("<tr bgcolor=$bordercolor>");
+echo("<td height=$borderwidth width=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("<td height=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("<td height=$borderwidth width=$borderwidth><img src=$top/images/pixel_trans.gif></td>");
+echo("</tr>");
+echo("</table>");
+
+if ($fitToWindow) {
+	echo "</div>";
+}
+?>
+
+<table border=0 width=<?=$mainWidth?> cellpadding=0 cellspacing=0>
+<!-- caption -->
+<tr>
+<td colspan=3 align=center>
+<span class="caption">
+<?= editCaption($album, $index, $edit) ?>
+</span>
+<br>
+<br>
+</td>
+</tr>
+
+<?
+echo("<tr><td colspan=3>");
+includeHtmlWrap("inline_photo.footer");
+echo("</td></tr>");
+?>
+
+</table>
+<table border=0 width=<?=$mainWidth?> cellpadding=0 cellspacing=0>
+<tr>
+<td>
+<?
+include("layout/navphoto.inc");
+$breadcrumb["top"] = false;
+include("layout/breadcrumb.inc");
+?>
+</td>
+</tr>
+</table>
+</center>
+<?
+includeHtmlWrap("photo.footer");
+?>
+
+</body>
+</html>
