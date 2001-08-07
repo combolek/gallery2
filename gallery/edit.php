bcd188d1b62f0bececcf90504009191269c322c8
jackodog <jackodog@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-08-07 07:41:48 +0000

    Now basically functional. At least less broken than before.

--- gallery/edit.php
+++ gallery/edit.php
@@ -20,6 +20,15 @@
 ?>
 <? 
 
+function makeEditUrl($type, $id, $t, $return) {
+	$url = "edit.php?"
+		. "type=$type"
+		. "&id=$id"
+		. "&tab=$t"
+		. "&return=" . urlencode($return);
+	return makeGalleryUrl($url);
+}
+
 require($GALLERY_BASEDIR . "init.php"); 
 
 $albumName = $gallery->session->albumName;
@@ -37,40 +46,129 @@ if (!$album->isLoaded()) {
     return;
 }
 
+if ($doreturn) {
+	header("Location: $return");
+	return;
+}
+
+if (!$tab) {
+	$tab = "general";
+}
+$editDir = $GALLERY_BASEDIR . "edit/";
+$editInclude = $editDir. $type . "_" . $tab . ".inc";
+
 //-- if we have a submit and the command wants to show progress ---
-//-- go straight there... ---
+//-- go straight there (don't bother loading the layout)...     ---
+//-- Otherwise we 'doit' within the layout ---
 if ($doit && $show_progress) {
 
 	echo ("<html><head><title>in progress</title></head>");
-	reload();
+
+	require($editInclude);
+
+	$url = makeEditUrl($type, $id, $tab, $return);
+    $doit = 0;
+    $show_progress = 0;
+	echo ("<BODY onLoad='document.location = \"$url\"'>");
 	
 }
 
 
+$commands = Array();
+$commandCount = 0;
 
 //-- load up the available commands ---
 switch ($type) {
 
 	case "album":
-		//
+
+		//-- the item info for display ---
+		$myAlbum = new Album();
+		$myAlbum->load($id);
+		$thumbnail['tag'] = $myAlbum->getHighlightTag(100);
+		$thumbnail['url'] = $album->getHighlightPath($index);
+
+        //-- the album commands ---
+		if ($user->canChangeTextOfAlbum($myAlbum)) {
+			$name = "general";
+			$commands[$name]['title'] = "Info";
+			$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+		}
+
 		break;
 
 	case "item":
-		//
+
+		//-- the item info for display ---
+		$index = $album->getPhotoIndex($id);
+		$thumbnail['tag'] = $album->getThumbnailTag($index, 100);
+		$thumbnail['url'] = $album->getThumbnailPath($index);
+
+		//-- the item commands ---
+		if ($user->canChangeTextOfAlbum($album)) {
+			$name = "general";
+			$commands[$name]['title'] = "Info";
+			$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+		}
+		if ($user->canWriteToAlbum($album)) {
+			if (!$album->isMovie($id)) {
+				$name = "image_thumbnail";
+				$commands[$name]['title'] = "Edit Thumbnail";
+				$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+				$name = "image_rotate";
+				$commands[$name]['title'] = "Rotate";
+				$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+			}
+			$name = "showhide";
+			$commands[$name]['title'] = "Show/Hide";
+			$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+
+        }
+
+		if ($user->canDeleteFromAlbum($album))
+			$name = "delete";
+			$commands[$name]['title'] = "Delete";
+			$commands[$name]['href'] = makeEditUrl($type, $id, $name, $return);
+
 		break;
+
 	default:
 		// uh, this is bad...
 }
 
 //-- which command is selected ---
+$commands[$tab]['selected'] = 1;
+
+//-- form elements ---
+$formUrl = makeGalleryUrl("edit.php");
+$form['openTag'] = "<form action=\"$formUrl\" method=\"POST\">";
+$form['closeTag'] = "</form>";
+$form['hidden'] = "
+	<input type=\"hidden\" name=\"type\" value=\"$type\">
+	<input type=\"hidden\" name=\"tab\" value=\"$tab\">
+	<input type=\"hidden\" name=\"id\" value=\"$id\">
+	<input type=\"hidden\" name=\"return\" value=\"$return\">
+";
+
+$form['buttons'] = "
+	<input type=\"submit\" name=\"doit\" value=\"Do It!\">
+	<input type=\"submit\" name=\"doreturn\" value=\"No More. I'm Done\">
+";
 
 
+//-- The Layout object ---
+$GLO['item']['id'] = $id;
+$GLO['item']['thumbnail'] = $thumbnail;
+$GLO['item']['thumbnail']['size'] = 100;
 
+$GLO['album']['fontColor'] = "black";
+$GLO['album']['bgColor'] = "white";
 
+$GLO['commands'] = $commands;
 
-//-- if show_progress is set, then we actually display an interum page instead
-//-- of the real one.
-
+//-- the tab content ---
+$GLO['form'] = $form;
+$GLO['selected']['content'] = include($editInclude);
 
 //-------------------------------------------------------------------------
 //-- The Layout of the Page ---
