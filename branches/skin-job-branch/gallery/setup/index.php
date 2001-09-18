7a8c66ab05d9a873abddb7fdfd778becd2aa4d91
jackodog <jackodog@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-09-18 03:56:47 +0000

    Merging changes from HEAD (1.2.1) to skin-job branch

--- branches/skin-job-branch/gallery/setup/index.php
+++ branches/skin-job-branch/gallery/setup/index.php
@@ -24,7 +24,7 @@ if (function_exists("posix_getpwuid")) {
 } else {
 	$whoami = locateFile("whoami");
 	if ($whoami) {
-		exec($whoami, $results);
+		fs_exec($whoami, $results, $status);
 		$webserver_user = $results[0];
 	} else {
 		$webserver_user = "unknown";
@@ -62,7 +62,15 @@ foreach (array_keys($preserve) as $key) {
 
 <form method=POST>
 
-<? include("$setup_page.inc"); ?>
+<?
+$legit = array("check", "constants", "defaults", "confirm", "write");
+if (in_array($setup_page, $legit)) {
+  include("$setup_page.inc");
+} else {
+  print "Security violation.\n";
+  exit;
+}
+?>
 
 <?
 function embed_hidden($key) {
