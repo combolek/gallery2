fccd0d39cf9ec82702fbf72bc4acd46fc5bf5668
nobody <nobody@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-11-22 09:56:56 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_2_3'.

---
+++ tags/RELEASE_1_2_3/gallery/errors/unconfigured.php
@@ -0,0 +1,24 @@
+<? require($GALLERY_BASEDIR . "errors/configure_instructions.php") ?>
+<html>
+<head>
+  <title>Gallery Configuration Error</title>
+  <?= getStyleSheetLink() ?>
+</head>
+<body>
+<center>
+<span class="title">
+Gallery has not been configured!
+</span>
+<p>
+<center>
+<table width=80%><tr><td>
+Gallery must be configured before you can use it.  First, you must put it
+into configuration mode.  Here's how:
+<?= configure("configure"); ?>
+<p>
+And then start the <a href="<?=$GALLERY_BASEDIR?>setup/index.php">Configuration Wizard</a>
+
+<? include($GALLERY_BASEDIR . "errors/configure_help.php"); ?>
+</table>
+</body>
+</html>
