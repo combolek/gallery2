2ad2fcbe7a9f0412c6857a3365943971d7297666
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2001-12-03 00:17:21 +0000

    Heavily optimized this block.  Now it scans once a day for public albums and caches that data.  Subsequent requests pick a random album from the cache and then display a non-hidden photo from that album.

--- branches/class-revolt-branch/gallery/contrib/block-random.php
+++ branches/class-revolt-branch/gallery/contrib/block-random.php
@@ -19,10 +19,17 @@
  */
 
 /*
- * Block-random version 1.1
+ * This block selects a random photo for display.  It will only display photos
+ * from albums that are visible to the public.  It will not display hidden
+ * photos.  
+ *
+ * Once a day (or whatever you set CACHE_EXPIRED to) we scan all albums and
+ * create a cache file listing each public album and the number of photos it
+ * contains.  For all subsequent attempts we use that cache file.  This means
+ * that if you change your albums around it may take a day before this block
+ * starts (or stops) displaying them.
  */
-?>
-<?
+
 // Hack prevention.
 if (!empty($HTTP_GET_VARS["GALLERY_BASEDIR"]) ||
 		!empty($HTTP_POST_VARS["GALLERY_BASEDIR"]) ||
@@ -30,40 +37,168 @@ if (!empty($HTTP_GET_VARS["GALLERY_BASEDIR"]) ||
 	print "Security violation\n";
 	exit;
 }
-?>
-<?
+
 require($GALLERY_BASEDIR . "init.php");
-?>
-<?
+
+if ($profile) {
+    $timer = time();
+}
+
 /* Initializing the seed */
 srand ((double) microtime() * 1000000);
 
-for ($i = 0; $i < 10; $i++) {
-	list($album, $index) = getRandomPhoto();
-	if ($album) {
-		break;
-	}
+define(CACHE_FILE, $gallery->app->albumDir . "/block-random.cache");
+define(CACHE_EXPIRED, 86400);
+
+// Check the cache file to see if it's up to date
+$rebuild = 1;
+if (fs_file_exists(CACHE_FILE)) {
+    $stat = fs_stat(CACHE_FILE);
+    $mtime = $stat[9];
+    if (time() - $mtime < CACHE_EXPIRED) {
+	$rebuild = 0;
+    }
 }
 
-if ($album) {
-	$id = $album->getPhotoId($index);
+if ($rebuild) {
+    scanAlbums();
+    saveCache();
+} else {
+    readCache();
+}
 
-	echo ""
-	     ."<a href=" .makeAlbumUrl($album->fields["name"], $id) .">"
-	     .$album->getThumbnailTag($index)
-	     ."</a>";
-	
-	$caption = $album->getCaption($index);
-	if ($caption) {
-		echo "<br><center>$caption</center>";
-	}
-	
-	echo "<br><center>From: "
-	     ."<a href=" .makeAlbumUrl($album->fields["name"]) .">"
-	     .$album->fields["title"]
-	     ."</a></center>";
+$album = chooseAlbum();
+
+if ($album) {
+    $index = choosePhoto($album);
+}
+
+if (isset($index)) {
+    $id = $album->getPhotoId($index);
+    echo ""
+	    ."<a href=" .makeAlbumUrl($album->fields["name"], $id) .">"
+	    .$album->getThumbnailTag($index)
+	    ."</a>";
+    
+    $caption = $album->getCaption($index);
+    if ($caption) {
+	echo "<br><center>$caption</center>";
+    }
+    
+    echo "<br><center>From: "
+	    ."<a href=" .makeAlbumUrl($album->fields["name"]) .">"
+	    .$album->fields["title"]
+	    ."</a></center>";
 } else {
 	print "No photo chosen.";
+}
+
+if ($profile) {
+    $elapsed = time() - $timer;
+    print "<br>Elapsed: $elapsed secs";
+}
+
+/*
+ * --------------------------------------------------
+ * Support functions
+ * --------------------------------------------------
+ */
+
+function saveCache() {
+    global $cache;
+    if ($fd = fs_fopen(CACHE_FILE, "w")) {
+	foreach ($cache as $key => $val) {
+	    fwrite($fd, "$key/$val\n");
+	}
+	fclose($fd);
+    }
+}
+
+function readCache() {
+    global $cache;
+    if ($fd = fs_fopen(CACHE_FILE, "r")) {
+	while ($line = fgets($fd, 4096)) {
+	    list($key, $val) = explode("/", $line);
+	    $cache[$key] = $val;
+	}
+	fclose($fd);
+    }
+}
+
+function choosePhoto($album) {
+    global $cache;
+
+    $count = $cache[$album->fields["name"]];
+    if ($count == 0) {
+	// Shouldn't happen
+	return null;
+    } else if ($count == 1) {
+	$choose = 1;
+    } else {
+	$choose = rand(1, $count);
+	$wrap = 0;
+	if ($album->isHidden($choose)) {
+	    $choose++;
+	    if ($choose > $album->numPhotos(1)) {
+		$choose = 1;
+		$wrap++;
+
+		if ($wrap = 2) {
+		    return null;
+		}
+	    }
+	}
+    }
+
+    return $choose;
+}
+
+function chooseAlbum() {
+    global $cache;
+
+    /*
+     * The odds that an album will be selected is proportional
+     * to the number of (visible) items in the album.
+     */
+
+    $total = 0;
+    foreach ($cache as $name => $count) {
+	if (!$choose) {
+	    $choose = $name;
+	}
+	
+	$total += $count;
+	if ($total != 0 && ($total == 1 || rand(1, $total) <= $count)) {
+	    $choose = $name;
+	}
+    }
+
+    if ($choose) {
+	$album = new Album();
+	$album->load($choose);
+	return $album;
+    } else {
+	return null;
+    }
+}
+
+function scanAlbums() {
+    global $cache;
+    global $gallery;
+
+    $cache = array();
+    $everybody = $gallery->userDB->getEverybody();
+    $albumDB = new AlbumDB();
+    foreach ($albumDB->albumList as $tmpAlbum) {
+	if ($everybody->canReadAlbum($tmpAlbum)) {
+	    $seeHidden = $everybody->canWriteToAlbum($tmpAlbum);
+	    $numPhotos = $tmpAlbum->numPhotos($seeHidden);
+	    $name = $tmpAlbum->fields["name"];
+	    if ($numPhotos > 0) {
+		$cache[$name] = $numPhotos;
+	    }
+	}
+    }
 }
 
 function getRandomPhoto() {
@@ -74,29 +209,6 @@ function getRandomPhoto() {
 	/* Try n times to get an album with photos in it */
 	for ($i = 0; $i < 10; $i++) {
 		$count = 0;
-		foreach ($albumDB->albumList as $tmpAlbum) {
-			if ($gallery->user->canReadAlbum($tmpAlbum)) {
-
-				/*
-				 * The odds that this album will be selected is proportional
-				 * to the number of (visible) items in the album.
-				 */
-				$seeHidden = $gallery->user->canWriteToAlbum($tmpAlbum);
-				$numPhotos = $tmpAlbum->numPhotos($seeHidden);
-				$count += $numPhotos;	
-
-				// print "Check {$tmpAlbum->fields[title]}<br>";
-				// print "Count is now: $count<br>";
-
-				if ($count != 0 && ($count == 1 || rand(1, $count) <= $numPhotos)) {
-					$album = $tmpAlbum;
-					// print "--> keep!<br>";
-				} 
-			}
-		}
-
-		// We'll skip hidden photos
-		$count = $album->numPhotos();
 
 		// debug
 		// print "Count: $count<br>";
