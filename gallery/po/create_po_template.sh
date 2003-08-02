a8d9050db9491f6b4e746031159e082220012ff2
nobody <nobody@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2003-08-02 06:52:18 +0000

    This commit was manufactured by cvs2svn to create branch 'BRANCH_dev_new_ui'.

--- gallery/po/create_po_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# $Id$
-
-#note: requires xgettext version 0.12.1 or greater
-
-echo '# $Id$' > gallery.pot
-
-xgettext --files-from=filelist -LPHP --keyword=_ --no-wrap -o - >> gallery.pot
