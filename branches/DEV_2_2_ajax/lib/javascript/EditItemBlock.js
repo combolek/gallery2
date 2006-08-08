3245c8cd927cbb5cf09188f5df14125756369645
jablko <jablko@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2006-08-08 17:55:45 +0000

    Change EditItemBlock from static to class methods

--- branches/DEV_2_2_ajax/lib/javascript/EditItemBlock.js
+++ branches/DEV_2_2_ajax/lib/javascript/EditItemBlock.js
@@ -20,76 +20,73 @@
 /**
  * Template's client-side variables & functions
  */
-var EditItemBlock = {
+function EditItemBlock() {
+}
 
-  /**
-   * Submit template's form using Ajax
-   *
-   * @param {event} event which triggered the form's submision
-   * @param {Object} self reference, since function will be executed in scope of the event target
-   */
-  submit: function(url, templateId, EditItemBlock) {
-    return function(event, self) {
-      if (!GalleryUtilities.isCallbackSupported()) {
-	return;
-      }
-
-      var form = YAHOO.util.Dom.get(GalleryUtilities.elementId("form", "EditItemBlock",
-        templateId));
-
-      /* Disable template's form */
-      YAHOO.util.Dom.batch(form.elements, function(element) {
-	element.blur();
-	element.disabled = "disabled";
-      });
-
-      /* Give immediate feedback if possible */
-      YAHOO.util.Dom.get(
-        GalleryUtilities.elementId("value", "EditItemBlock", templateId)).innerHTML = "Saving...";
-      GalleryUtilities.hide(GalleryUtilities.elementId("status", "EditItemBlock", templateId));
-      GalleryUtilities.hide(GalleryUtilities.elementId("form", "EditItemBlock", templateId));
-      GalleryUtilities.show(GalleryUtilities.elementId("link", "EditItemBlock", templateId));
-
-      var connection;
-
-      /* Register with all callback responses */
-      GalleryUtilities.callbackEvent.subscribe(function(type, args) {
-	var response = args[0];
-	if (response.tId == connection.tId) {
-
-	  /* Enable template's form */
-	  YAHOO.util.Dom.batch(form.elements, function(element) {
-	    element.disabled = "";
-	  });
-
-	  if (!GalleryUtilities.isResponseSuccessful(response)) {
-	    GalleryUtilities.show(GalleryUtilities.elementId("status", "EditItemBlock",
-	      templateId));
-	    GalleryUtilities.hide(GalleryUtilities.elementId("success", "EditItemBlock",
-	      templateId));
-	    GalleryUtilities.hide(GalleryUtilities.elementId("warning", "EditItemBlock",
-	      templateId));
-	    GalleryUtilities.show(GalleryUtilities.elementId("error", "EditItemBlock",
-	      templateId));
-	  }
-	}
-      });
-
-      /* Serialize form elements */
-      var args = GalleryUtilities.serializeForm(form, YAHOO.util.Event.getTarget(event));
-      args.push(GalleryUtilities.formVar("callback") + "=callback");
-
-      args.push(GalleryUtilities.formVar("delegate[view]") + "=core.EditItemBlock");
-      args.push(GalleryUtilities.formVar("delegate[templateId]") + "=" + templateId);
-      args.push(GalleryUtilities.formVar("delegate[EditItemBlock][item][id]") + "="
-        + EditItemBlock.item.id);
-      args.push(GalleryUtilities.formVar("delegate[EditItemBlock][property]") + "="
-        + EditItemBlock.property);
-
-      /* Make Ajax callback request */
-      connection = GalleryUtilities.callbackRequest(url, args);
-
-      YAHOO.util.Event.preventDefault(event);
-    };
+/**
+ * Submit template's form using Ajax
+ *
+ * @param {Object} scope from which to import new values
+ * @param {Element} target of event which triggered the form's submision
+ * @param {Object} self reference, since function will be executed in scope of the event target
+ */
+EditItemBlock.prototype.submit = function(EditItemBlock, target, self) {
+  if (!GalleryUtilities.isCallbackSupported()) {
+    return;
   }
-};
+
+  var form = YAHOO.util.Dom.get(GalleryUtilities.elementId("form", "EditItemBlock",
+    self.templateId));
+
+  /* Disable template's form */
+  YAHOO.util.Dom.batch(form.elements, function(element) {
+    element.blur();
+    element.disabled = "disabled";
+  });
+
+  /* Give immediate feedback if possible */
+  YAHOO.util.Dom.get(
+    GalleryUtilities.elementId("value", "EditItemBlock", self.templateId)).innerHTML = "Saving...";
+  GalleryUtilities.hide(GalleryUtilities.elementId("status", "EditItemBlock", self.templateId));
+  GalleryUtilities.hide(GalleryUtilities.elementId("form", "EditItemBlock", self.templateId));
+  GalleryUtilities.show(GalleryUtilities.elementId("link", "EditItemBlock", self.templateId));
+
+  var connection;
+
+  /* Register with all callback responses */
+  GalleryUtilities.responseEvent.subscribe(function(type, args) {
+    var response = args[0];
+    if (response.tId == connection.tId) {
+
+      /* Enable template's form */
+      YAHOO.util.Dom.batch(form.elements, function(element) {
+	element.disabled = "";
+      });
+
+      if (!GalleryUtilities.isResponseSuccessful(response)) {
+	GalleryUtilities.show(GalleryUtilities.elementId("status", "EditItemBlock",
+	  self.templateId));
+	GalleryUtilities.hide(GalleryUtilities.elementId("success", "EditItemBlock",
+	  self.templateId));
+	GalleryUtilities.hide(GalleryUtilities.elementId("warning", "EditItemBlock",
+	  self.templateId));
+	GalleryUtilities.show(GalleryUtilities.elementId("error", "EditItemBlock",
+	  self.templateId));
+      }
+    }
+  });
+
+  /* Serialize form elements */
+  var args = GalleryUtilities.serializeForm(form, target);
+  args.push(GalleryUtilities.formVar("callback") + "=callback");
+
+  args.push(GalleryUtilities.formVar("delegate[view]") + "=core.EditItemBlock");
+  args.push(GalleryUtilities.formVar("delegate[templateId]") + "=" + self.templateId);
+  args.push(GalleryUtilities.formVar("delegate[EditItemBlock][item][id]") + "="
+    + EditItemBlock.item.id);
+  args.push(GalleryUtilities.formVar("delegate[EditItemBlock][property]") + "="
+    + EditItemBlock.property);
+
+  /* Make Ajax callback request */
+  connection = GalleryUtilities.callbackRequest(form.action, args);
+}
