8d77f8903403852a57eb7470e120ccb6690ca6f8
ckdake <ckdake@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-09-16 14:14:49 +0000

    tagging 1.5.9 release

---
+++ tags/RELEASE_1_5_9/BRANCH_1_5_LEGACY/gallery/includes/definitions/navIcons.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Defines the navigation icons.
+ *
+ * @package Gallery
+ *
+ * $Id$
+*/
+
+if (!isset($gallery)) {
+	exit;
+}
+
+// Path is relative to the icons folder
+if ($gallery->direction == 'ltr') {
+	$fpImg = 'navigation/nav_first.gif';
+	$ppImg = 'navigation/nav_prev.gif';
+	$npImg = 'navigation/nav_next.gif';
+	$lpImg = 'navigation/nav_last.gif';
+}
+else {
+	$fpImg = 'navigation/nav_last.gif';
+	$ppImg = 'navigation/nav_next.gif';
+	$npImg = 'navigation/nav_prev.gif';
+	$lpImg = 'navigation/nav_first.gif';
+}
+?>
