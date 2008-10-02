8d1ada9c2350fd2fe37fe89b1072c6b09bfb88fa
tnalmdal <tnalmdal@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-02 00:23:05 +0000

    Initial import.  This is the Kohana framework that was used for the evaluation code.

---
+++ trunk/eval/gx/kohana/framework/system/libraries/CTI.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Object Relational Mapping library (ORM) is a method of abstracting database
+ * access to standard PHP calls. All table rows are represented as a model.
+ *
+ * @see http://en.wikipedia.org/wiki/Active_record
+ * @see http://en.wikipedia.org/wiki/Object-relational_mapping
+ *
+ * $Id: ORM.php 3304 2008-08-08 18:45:30Z Shadowhand $
+ * 
+ * This extension extends the base to support Class Table Inheritance.
+ *
+ * Based on:
+ *
+ * Wouter's Class Table Inhertenace rewrite
+ * @commnets   http://forum.kohanaphp.com/comments.php?DiscussionID=1074
+ *  *
+ * Extended by:
+ *
+ * @author     talmdal
+ * @commnets   Change the name and extend ORM_Core
+ *
+ */
+
+class CTI_Core extends ORM_Core {
+} // End ORM
+
+?>
