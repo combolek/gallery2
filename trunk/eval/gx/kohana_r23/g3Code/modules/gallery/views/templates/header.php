1d46b3577e7fd70430d64a18b3ee773d40875bdb
tnalmdal <tnalmdal@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-12 17:25:55 +0000

    A prototype version against the new Kohana R2.3 API - the application

---
+++ trunk/eval/gx/kohana_r23/g3Code/modules/gallery/views/templates/header.php
@@ -0,0 +1,35 @@
+
+<img id="Logo" alt="Logo" />
+
+<h1><?php echo $title; ?></h1>
+
+<a id="register-link" href="#">Register</a>
+
+<a id="login-link" href="#">Login</a>
+
+<ul id="global-navigation">
+    <?php foreach (array('Home/Dashboard' => '#',
+			 'Browse Photos' => 'show/album',
+			 'Upload New Photos' => 'upload',
+			 'Admin' =>  'admin',
+			 'Help?' => '#') as $text => $view):?>
+	<li>
+	  <?php if ($active == $text): ?>
+	    <a class="active" href="#"><?php echo $text; ?></a>
+	  <?php elseif ($view == '#'): ?>
+	    <a href="#"><?php echo $text; ?></a>
+	  <?php else: ?>
+	    <?php echo html::anchor($view, $text); ?>
+	  <?php endif; ?>
+	</li>
+    <?php endforeach; ?>
+</ul>
+
+<ul id="breadcrumbs">
+	<li class="root"><a href="#">Home</a></li>
+	<li><a href="#">Friends &amp; Family</a></li>
+
+	<li><a href="#">Christmas 2007</a></li>
+	<!-- later on the ">>" should be a background image -->
+</ul>
+
