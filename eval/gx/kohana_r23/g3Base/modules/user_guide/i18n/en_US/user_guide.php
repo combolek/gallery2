f1ab0cf59dee003d282dce21ea61219079f67286
andy_st <andy_st@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-18 07:12:01 +0000

    Retag G2.3 release

--- eval/gx/kohana_r23/g3Base/modules/user_guide/i18n/en_US/user_guide.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* $Id: user_guide.php 3326 2008-08-09 21:24:30Z Shadowhand $ */
-
-$lang = array
-(
-	'title'     => 'Kohana User Guide',
-	'copyright' => 'copyright (c) %s Kohana Team :: All rights reserved :: Rendered in {execution_time} seconds using {memory_usage}MB of memory',
-);
--- eval/gx/kohana_r23/g3Base/system/i18n/es_AR/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$lang = array
-(
-	'invalid_session_name' => 'El parametro session_name, %s, no es valido. Solo debe contener caracteres alfanumericos y guiones bajos. También al menos uno debe de ser una letra.',
-);
