2c545bb3dc4d79e0546188a0cc252100b75cfdb8
tnalmdal <tnalmdal@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-22 03:47:58 +0000

    Remove routing hack

--- eval/gx/kohana_r23/g3Base/system/classes/router.php
+++ eval/gx/kohana_r23/g3Base/system/classes/router.php
@@ -35,12 +35,6 @@ class Router_Core {
 	{
 		// Set the complete URI
 		self::$complete_uri = self::$current_uri.self::$query_string;
-		if (empty(self::$current_uri) && self::$query_string[0] == '?'){
-		    self::$current_uri = substr(self::$query_string, 2);
-		    $getKey = substr(self::$query_string, 1);
-		    unset($_GET[$getKey]);
-		    self::$query_string = '';
-		}
 		// Load routes
 		$routes = Kohana::config('routes');
 
