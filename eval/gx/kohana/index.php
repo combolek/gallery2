e3c4d28f03ad2353a3018fd116f98d569dd0d3f2
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-23 22:25:56 +0000

    Move /framework/system up to /kohana

--- eval/gx/kohana/index.php
+++ eval/gx/kohana/index.php
@@ -39,7 +39,7 @@ $kohana_modules = 'modules';
  *
  * This path can be absolute or relative to this file.
  */
-$kohana_system = 'framework/system';
+$kohana_system = 'kohana';
 
 /**
  * Test to make sure that Kohana is running on PHP 5.2 or newer. Once you are
