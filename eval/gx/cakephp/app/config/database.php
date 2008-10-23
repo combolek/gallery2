31987029c22275fa3f18d0c482f8ad9029c5ad25
thumb70 <thumb70@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-23 21:27:19 +0000

    moved ui folder out of kohana, removed redundant YUI script links

--- eval/gx/cakephp/app/config/database.php
+++ eval/gx/cakephp/app/config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'dyna2Mo',
 		'database' => 'gx_eval_cakephp',
 	);
 }
