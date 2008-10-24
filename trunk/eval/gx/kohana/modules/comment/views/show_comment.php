8e803e382dd0f9cbc7cf611a42569944d9847b81
thumb70 <thumb70@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-24 23:54:56 +0000

    Fixed new comment highlight, now pulses to highlight color and back to white.

--- trunk/eval/gx/kohana/modules/comment/views/show_comment.php
+++ trunk/eval/gx/kohana/modules/comment/views/show_comment.php
@@ -1,58 +1,55 @@
 <?php if ($comments): ?>
 <script type="text/javascript">
-	//var oElement = document.getElementById("gCommentAdd");
-	function addComment(e) {
-		var author, email, text, comment, li;
+  function addComment(e) {
+    var author, email, text, comment, li;
 
-		e.preventDefault(); // Prevent form submission
+    e.preventDefault(); // Prevent form submission
 
-		// Get form input
-		//oCommentForm = document.getElementById('gCommentAdd');
-		author = document.getElementById('gCommentAuthor').value;
-		email = document.getElementById('gCommentEmail').value;
-		text = document.getElementById('gCommentText').value;
-		//alert(author + email + text);
+    // Get form input
+    //oCommentForm = document.getElementById('gCommentAdd');
+    author = document.getElementById('gCommentAuthor').value;
+    email = document.getElementById('gCommentEmail').value;
+    text = document.getElementById('gCommentText').value;
 
-		// Create a new list element
-		var commentId = YAHOO.util.Dom.getElementsByClassName('gComment').length;
-		var li = document.createElement('li');
-		li.id = 'gComment-' + commentId;
-		li.className = 'gComment';
-		li.innerHTML = '<p><a href="' + email + '" class="gAuthor">' + author + '</a> ' +
-			' said 30 minutes ago <span class="gDate understate">(October 23, 2008 4:00pm)</span></p>' +
-			'<div class="gText">' + text + '</div>';
+    // Create a new list element
+    var commentId = YAHOO.util.Dom.getElementsByClassName('gComment').length;
+    var li = document.createElement('li');
+    li.id = 'gComment-' + commentId;
+    li.className = 'gComment';
+    li.innerHTML = '<p><a href="' + email + '" class="gAuthor">' + author + '</a> ' +
+      ' said 30 minutes ago <span class="gDate understate">(October 23, 2008 4:00pm)</span></p>' +
+      '<div class="gText">' + text + '</div>';
+  
+    // Append new list item with comment form input
+    var elCommentThread = new YAHOO.util.Element('gCommentThread');
 
-		// Append new list item with comment form input
-		var elCommentThread = new YAHOO.util.Element('gCommentThread');
+    // Append a list item to contain the new comment
+    elCommentThread.appendChild(li);
 
- 			// Append a list item to contain the new comment
- 			elCommentThread.appendChild(li);
-
-		// Highlight the new comment
-		var alertColor = {
-     			backgroundColor: { to: '#FCF1D2' }
-		};
+    // Highlight the new comment
+    var alertColor = new YAHOO.util.ColorAnim('gComment-' + commentId, {
+      backgroundColor: { to: '#FCF1D2' }
+    }, 1);
 
     var origColor = new YAHOO.util.ColorAnim('gComment-' + commentId, {
-    	backgroundColor: { to: '#fff' }
+      backgroundColor: { to: '#fff' }
     }, 1);
 
     // Chain the alert animation to reset to the original bg color
-    origColor.onComplete.subscribe(function() {
-      alertColor.animate();
+    alertColor.onComplete.subscribe(function() {
+      origColor.animate();
     });
 
-		var anim = new YAHOO.util.ColorAnim('gComment-' + commentId, alertColor)
-		anim.animate();
+    alertColor.animate();
 
-		// Send comment data to server
-		//var formObject = document.getElementById('gCommentAdd');
-		//YAHOO.util.Connect.setForm(formObject);
+    // Send comment data to server
+    //var formObject = document.getElementById('gCommentAdd');
+    //YAHOO.util.Connect.setForm(formObject);
 
-		// setForm is supposed to do this work for us, but.
-		var commentAuthor = document.getElementById('gCommentAuthor').value;
-		var commentEmail= document.getElementById('gCommentEmail').value;
-		var commentText = document.getElementById('gCommentText').value;
+    // setForm is supposed to do this work for us, but.
+    var commentAuthor = document.getElementById('gCommentAuthor').value;
+    var commentEmail= document.getElementById('gCommentEmail').value;
+    var commentText = document.getElementById('gCommentText').value;
     var itemId = document.getElementById('gItemId').value;
 
     var postData = 'author=' + commentAuthor;
@@ -60,24 +57,25 @@
     postData += '&text=' + commentText;
     postData += '&item_id=' + itemId;
 
-		var cObj = YAHOO.util.Connect.asyncRequest(
-		  'POST', 'index.php?/comment/Add', callback, postData);
-	}
-	YAHOO.util.Event.addListener("gCommentAdd", "submit", addComment);
+    var cObj = YAHOO.util.Connect.asyncRequest(
+      'POST', 'index.php?/comment/Add', callback, postData);
+    }
+    
+    YAHOO.util.Event.addListener("gCommentAdd", "submit", addComment);
 
-  var responseSuccess = function(o){
+    var responseSuccess = function(o){
+    
     // we can show some nice message here
-	};
+  };
 
-	var responseFailure = function(o){
+  var responseFailure = function(o){
     // handle error
-	}
+  }
 
-	var callback =
-	{
-	  success:responseSuccess,
-	  failure:responseFailure,
-	};
+  var callback = {
+    success:responseSuccess,
+    failure:responseFailure,
+  };
 </script>
 
 <div id="gComments">
@@ -86,35 +84,33 @@
     <?php foreach ($comments as $index=>$comment): ?>
     <li class="gComment" id="gComment-<?php print $index; ?>">
       <p>
-	<a href="#" class="gAuthor"> <?php print $comment->author ?></a>
-	said 2 hours ago <span class="gDate understate">(October 23, 2008 11:30am)</span>
+        <a href="#" class="gAuthor"> <?php print $comment->author ?></a>
+        said 2 hours ago <span class="gDate understate">(October 23, 2008 11:30am)</span>
       </p>
       <div>
-	<?php print $comment->text ?>
+        <?php print $comment->text ?>
       </div>
     </li>
     <?php endforeach; ?>
   </ul>
 
-  <form id="gCommentAdd">
+  <form id="gCommentAdd" class="gExpandedForm">
     <fieldset>
       <legend>Add comment</legend>
-      <div>
-	<label for="gCommentAuthor">Your Name</label>
-	<input type="text" id="gCommentAuthor" class="text" />
+      <div class="row">
+        <label for="gCommentAuthor">Your Name</label>
+        <input type="text" id="gCommentAuthor" class="text" />
       </div>
-      <div>
-	<label for="gCommentEmail">Your Email (not displayed)</label>
-	<input type="text" id="gCommentEmail" class="text" />
+      <div class="row">
+        <label for="gCommentEmail">Your Email (not displayed)</label>
+        <input type="text" id="gCommentEmail" class="text" />
       </div>
-      <div>
-	<label for="gCommentText">Comment</label>
-	<textarea id="gCommentText"></textarea>
+      <div class="row">
+        <label for="gCommentText">Comment</label>
+        <textarea id="gCommentText"></textarea>
       </div>
-
       <input type="hidden" id="gItemId" value="<?php print($item_id); ?>" />
       <input type="submit" id="gCommentSubmit" value="Add" />
-
     </fieldset>
   </form>
 
