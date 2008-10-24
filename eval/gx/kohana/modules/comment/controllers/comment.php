d61c33ea842dae9244167e8014becb4f080089e9
jozefs <jozefs@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-24 23:07:13 +0000

    Add support for saving comments to the database.

---
+++ eval/gx/kohana/modules/comment/controllers/comment.php
@@ -0,0 +1,32 @@
+<?php
+class Comment_Controller extends Gallery_Controller {
+  // Set the name of the template to use
+  public $template = 'templates/base';
+
+  public function Delete($commentId) {
+    $comment = ORM::factory('comment', $commentId);
+    $comment->delete();
+    url::redirect('data/index');
+  }
+
+  public function Add() {
+    Kohana::log('info', 'Adding new comment');
+
+    $author = $this->input->post('author');
+    $email = $this->input->post('email');
+    $text = $this->input->post('text');
+    $item_id = $this->input->post('item_id');
+
+    Kohana::log('info', print_r($this->input->post(), true));
+    Kohana::log('info', sprintf("author: %s, email: %s, text: %s, item_id: %d", $author, $email, $text, $item_id));
+    Kohana::log_save();
+
+    $comment = ORM::factory('comment');
+    $comment->author = $author;
+    $comment->email = $email;
+    $comment->text = $text;
+    $comment->item_id = $item_id;
+    $comment->save();
+  }
+}
+?>
--- eval/gx/kohana/modules/comment/views/show_comment.php
+++ eval/gx/kohana/modules/comment/views/show_comment.php
@@ -1,15 +1,11 @@
 <?php if ($comments): ?>
 <script type="text/javascript">
+	alert('test');
 	//var oElement = document.getElementById("gCommentAdd");
 	function addComment(e) {
 		var author, email, text, comment, li;
 
 		e.preventDefault(); // Prevent form submission
-
-		// Write comment to the database...
-
-		// Show the block
-		YAHOO.util.Dom.setStyle("gComment-3", 'display', 'block');
 
 		// Get form input
 		//oCommentForm = document.getElementById('gCommentAdd');
@@ -19,8 +15,10 @@
 		//alert(author + email + text);
 
 		// Create a new list element
+		var commentId = YAHOO.util.Dom.getElementsByClassName('gComment').length;
 		var li = document.createElement('li');
-		li.id = 'gComment-3';
+		li.id = 'gComment-' + commentId;
+		li.className = 'gComment';
 		li.innerHTML = '<p><a href="' + email + '" class="gAuthor">' + author + '</a> ' +
 			' said 30 minutes ago <span class="gDate understate">(October 23, 2008 4:00pm)</span></p>' +
 			'<div class="gText">' + text + '</div>';
@@ -35,17 +33,59 @@
 		var alertColor = {
      			backgroundColor: { to: '#FCF1D2' }
 		};
-		var anim = new YAHOO.util.ColorAnim('gComment-3', alertColor)
+
+    var origColor = new YAHOO.util.ColorAnim('gComment-' + commentId, {
+    	backgroundColor: { to: '#fff' }
+    }, 1);
+
+    // Chain the alert animation to reset to the original bg color
+    origColor.onComplete.subscribe(function() {
+      alertColor.animate();
+    });
+
+		var anim = new YAHOO.util.ColorAnim('gComment-' + commentId, alertColor)
 		anim.animate();
+
+		// Send comment data to server
+		//var formObject = document.getElementById('gCommentAdd');
+		//YAHOO.util.Connect.setForm(formObject);
+
+		// setForm is supposed to do this work for us, but.
+		var commentAuthor = document.getElementById('gCommentAuthor').value;
+		var commentEmail= document.getElementById('gCommentEmail').value;
+		var commentText = document.getElementById('gCommentText').value;
+    var itemId = document.getElementById('gItemId').value;
+
+    var postData = 'author=' + commentAuthor;
+    postData += '&email=' + commentEmail;
+    postData += '&text=' + commentText;
+    postData += '&item_id=' + itemId;
+
+		var cObj = YAHOO.util.Connect.asyncRequest(
+		  'POST', 'index.php?/comment/Add', callback, postData);
 	}
 	YAHOO.util.Event.addListener("gCommentAdd", "submit", addComment);
+
+  var responseSuccess = function(o){
+    // we can show some nice message here
+	};
+
+	var responseFailure = function(o){
+    // handle error
+	}
+
+	var callback =
+	{
+	  success:responseSuccess,
+	  failure:responseFailure,
+	};
 </script>
 
 <div id="gComments">
   <h2>Comments</h2>
   <ul id="gCommentThread">
-    <?php foreach ($comments as $comment): ?>
-    <li id="gComment-<?php print $comment->id ?>">
+    <?php foreach ($comments as $index=>$comment): ?>
+    <li class="gComment" id="gComment-<?php print $index; ?>">
       <p>
 	<a href="#" class="gAuthor"> <?php print $comment->author ?></a>
 	said 2 hours ago <span class="gDate understate">(October 23, 2008 11:30am)</span>
@@ -73,6 +113,7 @@
 	<textarea id="gCommentText"></textarea>
       </div>
 
+      <input type="hidden" id="gItemId" value="<?php print($item_id); ?>" />
       <input type="submit" id="gCommentSubmit" value="Add" />
 
     </fieldset>
