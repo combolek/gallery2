317c45dde4b10a53269f10b0713496039b3fa2bf
thumb70 <thumb70@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-25 01:45:02 +0000

    Added icons, fixed image references, added tag cloud and search form.

--- eval/gx/kohana/themes/default/views/templates/base.php
+++ eval/gx/kohana/themes/default/views/templates/base.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title><?php echo $header->title; ?></title>
@@ -11,36 +11,73 @@
     
       <div id="hd">
         <div id="gHeader">
-	      <?php echo $header; ?>
+        <?php echo $header; ?>
         </div>
       </div>
 
       <div id="bd">
       
-	    <div id="yui-main">
-	      <div id="gContent" class="yui-b">
-	        <?php echo $content; ?>
+        <div id="yui-main">
+          <div id="gContent" class="yui-b">
+            <?php echo $content; ?>
           </div>
         </div>
 
         <div id="gSidebar" class="yui-b">
         
-          <div id="gCarousel">
+          <form id="gSearch">
+            <fieldset>
+              <input type="text" class="text" value="Search Gallery ..." />
+              <input type="submit" class="submit" value="go" />
+            </fieldset>
+          </form>
+
+          <!-- div id="gCarousel">
             <h2>Carousel</h2>
-            <img src="images/carousel.png" />
+            <img src="themes/default/images/carousel.png" />
             <!-- for reference see http://developer.yahoo.com/yui/examples/carousel/csl_dynload2.html and 
-            	http://developer.yahoo.com/ypatterns/pattern.php?pattern=carousel -->
-          </div>
+              http://developer.yahoo.com/ypatterns/pattern.php?pattern=carousel -->
+          </div-->
           
-          <div id="gTagCloud">
+          <div class="gTagCloud">
             <h2>Tag cloud</h2>
-            <p>tag1, tag2, tag3, ...</p>
-            <form id="gCompactForm">
-              <fieldset>
-                <input type="text" name="gTags" id="gTags" class="text" value="add new tags ..." />
-                <input type="submit" name="" value="add" />
-              </fieldset>
-            </form>
+            <ul>
+              <li><a href="#" class="m size0">animation</a></li>
+              <li><a href="#" class="m size0">art</a></li>
+              <li><a href="#" class="m size1">blind</a></li>
+              <li><a href="#" class="m size3">blog</a></li>
+              <li><a href="#" class="m size1">bug-tracker</a></li>
+              <li><a href="#" class="m size2">bugs20</a></li>
+              <li><a href="#" class="m size0">canvas</a></li>
+              <li><a href="#" class="m size0">classification</a></li>
+              <li><a href="#" class="m size4">cocktail</a></li>
+              <li><a href="#" class="m size0">exhibtion</a></li>
+              <li><a href="#" class="m size0">forum</a></li>
+              <li><a href="#" class="m size1">geo-tagging</a></li>
+              <li><a href="#" class="m size0">german</a></li>
+              <li><a href="#" class="m size0">germany</a></li>
+              <li><a href="#" class="m size0">gläser</a></li>
+              <li><a href="#" class="m size0">graffiti</a></li>
+              <li><a href="#" class="m size0">illustration</a></li>
+              <li><a href="#" class="m size0">ITP</a></li>
+              <li><a href="#" class="m size0">javascript</a></li>
+              <li><a href="#" class="m size0">miami</a></li>
+              <li><a href="#" class="m size0">miknow</a></li>
+              <li><a href="#" class="m size0">nyc</a></li>
+              <li><a href="#" class="m size0">NYU</a></li>
+              <li><a href="#" class="m size0">ontology</a></li>
+              <li><a href="#" class="m size0">open-source</a></li>
+              <li><a href="#" class="m size0">project</a></li>
+              <li><a href="#" class="m size0">school-of-information</a></li>
+              <li><a href="#" class="m size0">screenshot</a></li>
+              <li><a href="#" class="m size0">shiftspace</a></li>
+              <li><a href="#" class="m size0">shop</a></li>
+              <li><a href="#" class="m size0">tagging</a></li>
+              <li><a href="#" class="m size2">talkingpoints</a></li>
+              <li><a href="#" class="m size0">university-of-michigan</a></li>
+              <li><a href="#" class="m size1">usability</a></li>
+              <li><a href="#" class="m size0">writing</a></li>
+            </ul>
           </div>
           
           <table class="gMetadata">
@@ -61,9 +98,9 @@
             </tbody>
           </table>
           
-        </div>
+        </div><!-- end gSidebar yui-b -->
         
-      </div><!-- end bd -->
+      </div><!-- end YUI bd -->
 
       <div id="ft">
         <div id="gFooter">
