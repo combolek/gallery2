1faa392e6100bd294f69ba4ea9d2fa8c626b5440
tnalmdal <tnalmdal@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-27 02:15:25 +0000

    Move user module data restore to user module

--- eval/gx/kohana/core/controllers/data.php
+++ eval/gx/kohana/core/controllers/data.php
@@ -26,7 +26,7 @@
  */
 class Data_Controller {
   function Index() {
-    foreach (array('core', 'Comment') as $module) {
+    foreach (array('core', 'Comment', 'User') as $module) {
       print html::anchor("data/reset/$module", "reset $module");
       print " | ";
       print html::anchor("data/populate/$module", "populate $module");
@@ -51,8 +51,6 @@ class Data_Controller {
               `rgt` int(9) default NULL,
               PRIMARY KEY  (`id`))
             ENGINE=InnoDB DEFAULT CHARSET=utf8;');
-
-      $this->_createAuthTables();
 
       $db->query('DROP TABLE IF EXISTS `sessions`;');
       $db->query('CREATE TABLE sessions (
@@ -128,62 +126,5 @@ class Data_Controller {
     if ($del_dir == TRUE AND $level > 0) {
       @rmdir($path);
     }
-  }
-
-  function _createAuthTables() {
-    $db = Database::instance('default');
-
-    $db->query('DROP TABLE IF EXISTS `roles_users`;');
-    $db->query('CREATE TABLE IF NOT EXISTS `roles_users` (
-                `user_id` int(10) unsigned NOT NULL,
-                `role_id` int(10) unsigned NOT NULL,
-                PRIMARY KEY  (`user_id`,`role_id`),
-                KEY `fk_role_id` (`role_id`)
-              ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
-
-    $db->query('DROP TABLE IF EXISTS `user_tokens`;');
-    $db->query('CREATE TABLE IF NOT EXISTS `user_tokens` (
-                  `id` int(11) unsigned NOT NULL auto_increment,
-                  `user_id` int(11) unsigned NOT NULL,
-                  `user_agent` varchar(40) NOT NULL,
-                  `token` varchar(32) NOT NULL,
-                  `created` int(10) unsigned NOT NULL,
-                  `expires` int(10) unsigned NOT NULL,
-                  PRIMARY KEY  (`id`),
-                  UNIQUE KEY `uniq_token` (`token`),
-                  KEY `fk_user_id` (`user_id`)
-                ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;');
-
-    $db->query('DROP TABLE IF EXISTS `roles`;');
-    $db->query('CREATE TABLE  `roles` (
-              `id` int(11) unsigned NOT NULL auto_increment,
-              `name` varchar(32) NOT NULL,
-              `description` varchar(255) NOT NULL,
-              PRIMARY KEY  (`id`),
-              UNIQUE KEY `uniq_name` (`name`))
-            ENGINE=InnoDB DEFAULT CHARSET=utf8;');
-
-    $db->query('DROP TABLE IF EXISTS `users`;');
-    $db->query('CREATE TABLE IF NOT EXISTS `users` (
-                `id` int(11) unsigned NOT NULL auto_increment,
-                `email` varchar(127) NOT NULL,
-                `username` varchar(32) NOT NULL default \'\',
-                `password` char(50) NOT NULL,
-                `logins` int(10) unsigned NOT NULL default \'0\',
-                `last_login` int(10) unsigned,
-                PRIMARY KEY  (`id`),
-                UNIQUE KEY `uniq_username` (`username`),
-                UNIQUE KEY `uniq_email` (`email`)
-              ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;');
-
-    $db->query('ALTER TABLE `roles_users`
-                    ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`)
-                        REFERENCES `users` (`id`) ON DELETE CASCADE,
-                    ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`)
-                        REFERENCES `roles` (`id`) ON DELETE CASCADE;');
-
-    $db->query('ALTER TABLE `user_tokens`
-                    ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`)
-                        REFERENCES `users` (`id`) ON DELETE CASCADE;');
   }
 }
