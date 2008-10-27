19f53bf7b766bef7bd00d9edb73a4978a09b789d
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-27 09:36:30 +0000

    Add basic conflict resolution so that the scaffolding code can add lots and lotsa images.

--- eval/gx/kohana/core/helpers/photo.php
+++ eval/gx/kohana/core/helpers/photo.php
@@ -5,15 +5,27 @@ class Photo_Core {
     $item->type = ItemType::Photo;
     $item->title = $title;
     $item->path = basename($filename);
-    $item->parent_id = $parent_id;
-    $item->parent->add_child($item);
 
+    // Conflict?  Rename the file to a random number
+    $retries = 100;
+    while (ORM::Factory('item')
+	   ->where('parent_id', $parent_id)
+	   ->where('path', $item->path)
+	   ->find()->id) {
+      $pi = pathinfo(basename($filename));
+      $item->path = rand() . "." . $pi['extension'];
+      if (!$retries--) {
+	throw new Exception("CONFLICT");
+      }
+    }
+
+    $item->parent_id = $parent_id;
     $image = Image::factory($filename);
     $image->save(DOCROOT . item::url($item));
 
+    $item->parent->add_child($item);
     $image->resize(200, 140, Image::WIDTH)
       ->save(DOCROOT . "var/thumbnails/{$item->id}.jpg");
-    $item->parent->add_child($item);
 
     return $item;
   }
