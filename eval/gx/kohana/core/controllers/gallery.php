0c074c1d543472ddaa6bf04ec6b851e904bd0045
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-29 22:48:17 +0000

    Improve the scaffolding to allow us to create a fake MPTT tree and perform operations on it, while profiling the database queries.

--- eval/gx/kohana/core/controllers/gallery.php
+++ eval/gx/kohana/core/controllers/gallery.php
@@ -8,16 +8,22 @@ class Gallery_Controller extends Template_Controller {
     $this->template->header->title = 'John Doe\'s Gallery';
 
     $this->template->header->is_admin = Auth::instance()->logged_in("admin");
-	$this->template->header->authenticated = Auth::instance()->logged_in();
+    $this->template->header->authenticated = Auth::instance()->logged_in();
 
-	$this->template->footer = new View('templates/footer.html');
+    $this->template->footer = new View('templates/footer.html');
 
+    try {
+      if (Session::instance()->get("use_profiler", false)) {
 	Event::add('system.display', array('Gallery_Controller', '_show_profile'));
+      }
+    } catch (Exception $e) {
+      // Perhaps the database is not initialized yet?
     }
+  }
 
-    static function _show_profile() {
-      $profiler = new Profiler();
-      $profiler->render();
-    }
+  static function _show_profile() {
+    $profiler = new Profiler();
+    $profiler->render();
+  }
 }
 ?>
