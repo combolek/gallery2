44970eb9fe5fc3ea96164de6563bc109e813c15b
tnalmdal <tnalmdal@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-10-30 00:37:47 +0000

    The start of the manage roles and user pages in the admin area.  Doesn't do much except list the current ones.

--- eval/gx/kohana/modules/user/views/admin_role.html.php
+++ eval/gx/kohana/modules/user/views/admin_role.html.php
@@ -2,4 +2,29 @@
 
 <h1>Admin: Roles</h1>
 
-<p> <?= $data; ?> </p>
+<table>
+  <thead>
+    <tr>
+      <td>&nbsp;</td>
+      <td>Role</td>
+      <td>Description</td>
+    </tr>
+  </thead>
+  <tbody>
+    <? foreach ($roles as $role): ?>
+      <tr>
+        <td>
+          <? if ($role->id > 2): ?>
+            <input type="checkbox" value="<?=$role->id; ?>">
+          <? else: ?>
+            &nbsp;
+          <? endif;?>
+        </td>
+        <td><?=$role->name; ?></td>
+        <td><?=$role->description ?></td>
+      </tr>
+    <? endforeach;?>
+  </tbody>
+  
+</table>
+
--- eval/gx/kohana/modules/user/views/admin_user.html.php
+++ eval/gx/kohana/modules/user/views/admin_user.html.php
@@ -2,4 +2,41 @@
 
 <h1>Admin: Users</h1>
 
-<p> <?= $data; ?> </p>
+<table>
+  <thead>
+    <tr>
+      <td>&nbsp;</td>
+      <td>User Name</td>
+      <td>Email</td>
+      <td>Login Count</td>
+      <td>Last Login</td>
+      <td>Roles</td>
+    </tr>
+  </thead>
+  <tbody>
+    <? foreach ($users as $user): ?>
+      <tr>
+        <td>
+          <? if ($user->id != 1): ?>
+            <input type="checkbox" value="<?=$user->id; ?>">
+          <? else: ?>
+            &nbsp;
+          <? endif;?>
+        </td>
+        <td><?=$user->username; ?></td>
+        <td><?=$user->email ?></td>
+        <td><?=$user->logins ?></td>
+        <td><?=date("D M j Y g:i:s", $user->last_login) ?></td>
+        <td>
+          <ul>
+            <? foreach ($user->roles as $role):?>
+              <li><?=$role->name; ?></li>
+            <? endforeach;?>
+          </ul>
+        </td>
+      </tr>
+    <? endforeach;?>
+  </tbody>
+  
+</table>
+
