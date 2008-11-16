c0e36a0d4ed663eabdbb7284cb36c1f23618fcc6
bharat <bharat@57fcd75e-5312-0410-8df3-f5eb6fbb1595>
2008-11-16 10:25:49 +0000

    Update php style for Gallery3

--- trunk/docs/dev/php-style.el
+++ trunk/docs/dev/php-style.el
@@ -1,34 +1,31 @@
 ; =============================================================================
 ;
-; Gallery - a web based photo album viewer and editor 
-; Copyright (C) 2000-2005 Bharat Mediratta 
-;  
-; This program is free software; you can redistribute it and/or modify 
-; it under the terms of the GNU General Public License as published by 
-; the Free Software Foundation; either version 2 of the License, or (at 
-; your option) any later version. 
-;  
-; This program is distributed in the hope that it will be useful, but 
-; WITHOUT ANY WARRANTY; without even the implied warranty of 
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-; General Public License for more details. 
-;  
-; You should have received a copy of the GNU General Public License 
-; along with this program; if not, write to the Free Software 
-; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+; Gallery - a web based photo album viewer and editor
+; Copyright (C) 2000-2008 Bharat Mediratta
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or (at
+; your option) any later version.
+;
+; This program is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ;
 ; =============================================================================
 ;
 ; To use this file, you must first install php-mode.el from
 ;     http://sourceforge.net/projects/php-mode
 ;
-;
 ; This file sets the indentation style for PHP code to match
-; the coding standard for the Gallery project, which can be 
+; the coding standard for the Gallery project, which can be
 ; found here:
-;
-;    http://sourceforge.net/docman/display_doc.php?docid=7664&group_id=7130
-;
+;    http://codex.gallery2.org/Gallery3:Coding_Standards
 ;
 
 ; Bind file suffixes to php-mode
@@ -46,17 +43,17 @@
 (autoload 'php-mode "php-mode" "PHP editing mode." t)
 
 ;;
-;
 ; Set up my PHP code convention compliant style.  This is very
 ; similar to java-style that comes with GNUEmacs 20.4
 ;
 (defconst php-gallery-style
   '(
-    (c-basic-offset . 4)
-    (my-c-continuation-offset . 8)
+    (c-basic-offset . 2)
+    (my-c-continuation-offset . 2)
     (c-comment-only-line-offset . (0 . 0))
     (c-offsets-alist . ((inline-open . 0)
-			(topmost-intro-cont    . 0)
+			(topmost-intro         . 0)
+			(topmost-intro-cont    . +)
 			(statement-block-intro . +)
 			(knr-argdecl-intro     . 5)
 			(substatement-open     . +)
@@ -67,6 +64,8 @@
 			(statement-case-open   . +)
 			(statement-cont        . +)
 			(access-label          . 0)
+			(defun-block-intro     . +)
+			(defun-close           . 0)
 			(arglist-cont-nonempty . c-lineup-arglist-intro-after-paren)
 			))
     ; (c-echo-syntactic-information-p . t)     ; turn this on to get debug info
@@ -75,57 +74,16 @@
 
 ;;
 ;
-; Create a hook that adds the style to the list and 
+; Create a hook that adds the style to the list and
 ; sets a few useful variables.
 ;
-(defun my-c-mode-common-hook ()
+(defun my-php-mode-hook ()
   (c-add-style "php-gallery" php-gallery-style t)
-  (turn-on-auto-fill)                 ; turn on auto-fill by default
+  (turn-on-auto-fill)
   (setq fill-column 98)
-  (setq comment-column 50)            ; set default comment column
+  (setq comment-column 50)
+  (setq comment-style 'box-multi)
+  (setq indent-tabs-mode nil)
   (define-key c-mode-map "\C-m" 'newline-and-indent)
   )
-
-;;
-;
-; Add my hook to the c-mode hook list
-;
-; These colors are designed to work with an ANSI terminal emulator
-; that only supports 16 colors.  The color names below are simply
-; indexes; the terminal remaps them whatever color the end user wants.
-; 
-; You'll probably want to change them to suit your taste.
-;
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-
 (add-hook 'php-mode-hook 'my-php-mode-hook)
-(defun my-php-mode-hook ()
-  (require 'font-lock)
-  (turn-on-font-lock)
-  (set-face-highlight-p 'font-lock-type-face nil)
-  (set-face-foreground 'font-lock-type-face "green")
-
-  (set-face-highlight-p 'font-lock-function-name-face nil)
-  (set-face-underline-p 'font-lock-function-name-face nil)
-  (set-face-foreground 'font-lock-function-name-face "blue")
-
-  (set-face-highlight-p 'font-lock-keyword-face nil)
-  (set-face-foreground 'font-lock-keyword-face "magenta")
-
-  (set-face-highlight-p 'font-lock-comment-face nil)
-  (set-face-foreground 'font-lock-comment-face "brightwhite")
-
-  (set-face-underline-p 'font-lock-variable-name-face nil)
-  (set-face-foreground 'font-lock-variable-name-face "yellow")
-
-  (set-face-foreground 'font-lock-reference-face "cyan")
-  (set-face-foreground 'font-lock-doc-string-face "x")
-
-  (set-face-underline-p 'font-lock-preprocessor-face nil)
-  (set-face-foreground 'font-lock-preprocessor-face "cyan")
-
-  (set-face-highlight-p 'font-lock-string-face nil)
-  (set-face-foreground 'font-lock-string-face "red")
-
-  (set-face-foreground 'font-lock-warning-face "x"))
-(setq font-lock-maximum-decoration t)
